f9bc822a72204c9426e795c43d0c4c248ed0dbce
Michał Wiącek <thepowerofra@gmail.com>
2019-09-08 16:52:23 +0200

    navbar scaffold

---
+++ containers/Landing/Banner/banner.style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const BannerWrapper = styled.section`
+  position: relative;
+  background-color: #030b16;
+  display: flex;
+  align-items: center;
+  padding-top: 80px;
+  align-items: flex-end;
+  @media (min-width: 991px) {
+    min-height: 100vh;
+  }
+  
+  .image-area {
+    @media (max-width: 767px) {
+    display: none;
+    }
+  }
+  `;
+
+export default BannerWrapper;
