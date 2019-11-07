1a86fa6c22367745b334e571f5b202f89047dfe8
michal <thepowerofra@gmail.com>
2019-11-07 02:13:19 +0100

    blogIndex is responsive

--- components/BlogPost/index.js
+++ components/BlogPost/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const BlogPost = ({ className, author, authorUrl, category, categoryUrl, thumbUrl, title, excerpt, id }) => {
     // Add all classes to an array
     const addAllClasses = ['blog_post'];
-
+    const ImageStyle = styled.div`
+        z-index: 9;
+        background-size: cover;
+        opacity: 0;
+        display: block;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        background-position: center center;
+        transition: all 0.25s cubic-bezier(0.785, 0.135, 0.15, 0.86) 0s;
+    `;
     return (
         <div className={addAllClasses.join(' ')}>
-            <div className="thumbnail">
-                <img src={thumbUrl} alt={title} />
-            </div>
+            <a href={title}>
+                <div className="thumbnail">
+                    <img src={thumbUrl} alt={title} />
+                    <ImageStyle style={{ backgroundImage: `url(${thumbUrl})` }} />
+                </div>
+            </a>
             <div className="text">
                 <a href={categoryUrl}>
                     <h5>{category}</h5>
                 </a>
-                <a href="title">
+                <a href={title}>
                     <h3 className="title">
                         <span className="hover-line">{title}</span>
                     </h3>
--- containers/Blog/BlogSection/index.js
+++ containers/Blog/BlogSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import Link from 'next/link';
 import Container from '../../../components/Container';
 import RecentStories from '../RecentStories';
-
 import { posts, featuredPost } from '../../../common/src/data';
 import SectionWrapper, { FeaturedArea, PostArea } from './blogSection.style';
 
@@ -11,19 +11,23 @@ const BlogSection = () => {
             <Container width="1260px">
                 <FeaturedArea>
                     <div className="featured_post">
-                        <div className="thumbnail">
-                            <img src={featuredPost.thumbUrl} alt={featuredPost.title} />
-                        </div>
-                        <div className="text">
+                        <Link href="/blog/[id]" as={`/blog/${featuredPost.id}`} key={`key-${featuredPost.id}`}>
+                            <div className="image-wrapper">
+                                <img src={featuredPost.thumbUrl} alt={featuredPost.title} />
+                            </div>
+                        </Link>
+                        <div className="copy-wrapper">
                             <a href={featuredPost.categoryUrl}>
                                 <h5>{featuredPost.category}</h5>
                             </a>
-                            <a href="title">
-                                <h3 className="title">
-                                    <span className="hover-line">{featuredPost.title}</span>
-                                </h3>
-                                <p className="excerpt">{featuredPost.excerpt}</p>
-                            </a>
+                            <Link href="/blog/[id]" as={`/blog/${featuredPost.id}`} key={`key-${featuredPost.id}`}>
+                                <a href="title">
+                                    <h3 className="title">
+                                        <span className="hover-line">{featuredPost.title}</span>
+                                    </h3>
+                                    <p className="excerpt">{featuredPost.excerpt}</p>
+                                </a>
+                            </Link>
                             <h6 className="hover-fade">
                                 By&nbsp;<a href={featuredPost.authorUrl}>{featuredPost.author}</a>
                             </h6>
